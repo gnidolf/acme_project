e93365493193dd60d030586e0dd8a8c58125590e
Никита Шабаев <shabaeff.nikita@yandex.ru>
2024-02-15 00:07:24 +0400

    ???

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = ('first_name',
+                    'last_name',
+                    'birthday',)
+    
+admin.site.register(Birthday, BirthdayAdmin)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 
+from .forms import BirthdayForm
+from .models import Birthday
+from .utils import calculate_birthday_countdown
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.POST or None)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+            )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
