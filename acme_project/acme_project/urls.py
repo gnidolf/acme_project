ef9a6b00901aefd6f2f6c453b28ec191826e16d7
gnidolf <shabaev43@gmail.com>
2024-03-14 15:26:42 +0300

    registration

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls), 
     path('birthday/', include('birthday.urls')),
-]
+    ]
